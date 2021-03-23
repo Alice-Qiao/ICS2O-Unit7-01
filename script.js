ae8807ddc403eed2f997b849bd248c00c761f4a1
AliceQiao <alice.qiao@ocsbstudent.ca>
2021-03-23 17:03:19 +0000

    spaces

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const computer = Math.floor(Math.random() * 7) + 1
 // that sets the two numbers. one will be the value input by the user. the other will be randominised from 1- 6
 
 document.getElementById('button').addEventListener('click', compare)
-//will activate function compare when button is clicked
+// will activate function compare when button is clicked
 
 function compare () {
   if (number === computer) {
     document.getElementById('congrats').style.display = 'block'
-} else {
+  } else {
     document.getElementById('sorry').style.display = 'block'
- }
+  }
 }
-//function means that if the two number as equal, it will show the congratulatory statement. If not, it will show the sorry statment
+// function means that if the two number as equal, it will show the congratulatory statement. If not, it will show the sorry statment
