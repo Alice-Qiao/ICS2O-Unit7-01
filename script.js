1b38a0a2656e85e773eec0ad3b4641947db8380a
AliceQiao <alice.qiao@ocsbstudent.ca>
2021-03-23 16:45:43 +0000

    minor tweaks

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let number = document.getElementById('guess').value
-let computer = Math.floor(Math.random() * 6) + 1;
+let computer = Math.floor(Math.random() * 7) + 1;
+// that sets the two numbers. one will be the value input by the user. the other will be randominised from 1- 6
 
 document.getElementById('button').addEventListener('click', compare)
 
 function compare () {
- if (number == computer) {
+ if (number === computer) {
    document.getElementById('congrats').style.display = 'block'
  } else {
    document.getElementById('sorry').style.display = 'block'
